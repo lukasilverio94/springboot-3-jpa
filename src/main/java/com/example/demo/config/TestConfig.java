9d1dcbbc55b0f47e02689775207374772cc4f0fd
= <lukasilverio94@gmail.com>
2025-03-24 23:48:24 +0100

    Payment entity, OneToOne association with Order

--- src/main/java/com/example/demo/config/TestConfig.java
+++ src/main/java/com/example/demo/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.example.demo.entities.Order;
 import com.example.demo.entities.OrderItem;
+import com.example.demo.entities.Payment;
 import com.example.demo.entities.Product;
 import com.example.demo.entities.User;
 import com.example.demo.entities.Category;
@@ -82,5 +83,11 @@ public class TestConfig implements CommandLineRunner {
         OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
         orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+        Payment pay1 = new Payment(null,  Instant.parse("2019-06-20T21:53:07Z"), o1);
+        // OneToOne Payment with Order
+        // We won't use the Payment repository, instead:
+        o1.setPayment(pay1);
+        orderRepository.save(o1); // Save again, now with a Payment 
     }
 }
