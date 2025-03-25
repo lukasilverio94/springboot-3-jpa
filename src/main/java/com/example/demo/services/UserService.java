8bc8f77e82e48505307cfc9c63e579decb65a14b
= <lukasilverio94@gmail.com>
2025-03-25 16:41:14 +0100

    crud: Delete user func

--- src/main/java/com/example/demo/services/UserService.java
+++ src/main/java/com/example/demo/services/UserService.java
@@ -15,16 +15,20 @@ public class UserService {
     @Autowired
     private UserRepository repository;
 
-    public List<User> findAll(){
+    public List<User> findAll() {
         return repository.findAll();
     }
 
-    public User findById(Long id){
-     Optional<User> obj = repository.findById(id);
-     return obj.get();
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
     }
 
-    public User insert(User obj){
-       return repository.save(obj);
+    public User insert(User obj) {
+        return repository.save(obj);
+    }
+
+    public void delete(Long id) {
+        repository.deleteById(id);
     }
 }
